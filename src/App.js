daf97f1fe2081e782062fb74608a5fddca92f3d0
Sam <sameerkhanmd112@gmail.com>
2021-12-06 13:58:49 -0800

    added states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,83 @@
 
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
 const { io } = require('socket.io-client')
 
 function App() {
+
+  const [data, setData] = useState([], () => {
+    console.log('data STATE:', data)
+  })
+
+  const [recorder, setRecorder] = useState(() => {
+    return new MediaRecorder(new MediaStream())
+  })
+
+  const socket = useMemo(() => {
+
+    let socket = io('ws://localhost:3000/');
+
+    socket.on("getblobs", async (d) => {
+
+      async function set(){
+        const dd = [...data, ...d]
+        setData(dd)
+      }
+      await set()
+    })
+
+    return socket
+  }, [data])
+  
+
+  useEffect(()=>{
+
+    console.log('use Effect-----', data)
+    if(data.length !== 0){
+      var blob = new Blob(data, {
+        type: "video/webm"
+      });
+      
+      const video = document.getElementById('--screencapture-record')
+    
+      video.src= URL.createObjectURL(blob)
+    }
+  }, [data])
+
+
+
+  const startRecording = async () => {
+
+    const video = document.getElementById('--screencapture-element');
+    let stream = video.srcObject
+    const rec = new MediaRecorder(stream)
+  
+    rec.ondataavailable = event => {
+      socket.emit("blobdata", event.data)
+    }
+
+    setRecorder(rec)
+    rec.start(10);
+  }
+
+  const stopRecording = async () => {
+    const lengthInMS = 100
+    let stopped = new Promise((resolve, reject) => {
+      recorder.onstop = resolve;
+      recorder.onerror = event => reject(event.name);
+    });
+    
+    console.log('state:   ', recorder.state)
+    let recorded = wait(lengthInMS).then(
+      () => recorder.state === "recording" && recorder.stop()
+    );
+    
+    await Promise.all([
+      stopped,
+      recorded
+    ]);
+  }
   return (
 
     <div className="h-screen flex flex-col">
@@ -40,9 +114,9 @@ function App() {
           <video width="500"   autoPlay id="--screencapture-element"/>
         </div>
         <div className="inline-block ">
-          <button type="button" onClick={playRecord} className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+          {/* <button type="button" onClick={playRecord} className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
               Play Record
-          </button>
+          </button> */}
           <video width="500" autoPlay id="--screencapture-record"/>
         </div>
       </div>
@@ -50,25 +124,6 @@ function App() {
   );
 }
 
-let socket = io('ws://localhost:3000/');
-
-let count = 0;
-async function playRecord(){
-  // var blob = new Blob(data, {
-  //   type: "video/webm"
-  // });
-  
-  // const video = document.getElementById('--screencapture-record')
-
-  // video.src= URL.createObjectURL(blob)
-
-  // const socket = io("ws://localhost:3000");
-  socket.emit("hello", "im am sameer"+ count)
-  count++
-
-  socket.emit('blobdata', data)
-  
-}
 
 async function screenCapture(){
   const video = document.getElementById('--screencapture-element')
@@ -81,7 +136,7 @@ async function screenCapture(){
       sampleRate: 44100,
     },
   });
-  console.log(video.srcObject.getVideoTracks())
+  // console.log(video.srcObject.getVideoTracks())
 }
 
 function stopCapture(){
@@ -96,48 +151,4 @@ function wait(delayInMS) {
   return new Promise(resolve => setTimeout(resolve, delayInMS));
 }
 
-let data = [];
-
-
-let recorder;
-async function startRecording() {
-  // const socket = io("ws://localhost:3000/");
-
-
-  let lengthInMS = 100
-
-  const video = document.getElementById('--screencapture-element');
-  let stream = video.srcObject
-
-  recorder = new MediaRecorder(stream);
-
-  recorder.ondataavailable = event => {
-    console.log(event.data)
-    data.push(event.data);
-  }
-
-  recorder.start(10);
-  console.log(recorder.state + " for " + (lengthInMS/1000) + " seconds...");
-
-  return data;
-}
-
-async function stopRecording(){
-
-  const lengthInMS = 100
-  let stopped = new Promise((resolve, reject) => {
-    recorder.onstop = resolve;
-    recorder.onerror = event => reject(event.name);
-  });
-
-  let recorded = wait(lengthInMS).then(
-    () => recorder.state === "recording" && recorder.stop()
-  );
-  
-  console.log(data)
-  await Promise.all([
-    stopped,
-    recorded
-  ]);
-}
 export default App;
